42c4a6f673ed6cb81e3d8138b45b84a5a235925c
Zaiter <azaiter@svsu.edu>
2017-11-14 13:39:14 -0800

    zaiter: add api

---
+++ api/app.py
@@ -0,0 +1,41 @@
+'''
+Dependencies:
+	install python 2.7.14
+	install latest pip
+	install flask
+	install flask-restful
+	install mysqlclient
+	install validate_email
+	install Flask-HTTPAuth
+'''
+# import dependencies
+from sys import argv
+from flask import Flask
+from flask_restful import Api
+
+# routing classes
+from resources.index import index
+from resources.helloworld import helloworld
+from resources.user import user
+from resources.login import login
+from resources.register import register
+from resources.createproject import createproject
+from resources.getprojects import getprojects
+
+# define the app and run it
+app = Flask(__name__)
+api = Api(app)
+
+# routing paths
+api.add_resource(index, '/')
+api.add_resource(helloworld, '/helloworld', '/helloworld/')
+api.add_resource(user, '/user', '/user/')
+api.add_resource(login, '/user/login', '/user/login/')
+api.add_resource(register, '/user/create', '/user/create/')
+api.add_resource(createproject, '/project/create', '/project/create/')
+api.add_resource(getprojects, '/project/get', '/project/get/')
+
+# for debugging, running indivisually with -debug param
+if '-debug' in argv:
+	app.run()
+#app.run()
---
+++ api/resources/createproject.py
@@ -0,0 +1,29 @@
+from flask_restful import Resource, reqparse
+from __init__ import dbengine, managemeutil
+
+post_parser = reqparse.RequestParser(bundle_errors=True)
+
+post_parser.add_argument(
+    'token', dest='token',
+    location='form', required=True,
+    type=managemeutil.verify_request_token,
+    help='The user\'s token {error_msg}',
+)
+
+post_parser.add_argument(
+    'project_name', dest='project_name',
+    location='form', required=True,
+    type=str,
+    help='The project\'s name {error_msg}',
+)
+post_parser.add_argument(
+    'project_desc', dest='project_desc',
+    location='form', required=True,
+    type=str,
+    help='The project\'s description {error_msg}',
+)
+
+class createproject(Resource):
+  def post(self):
+    args = post_parser.parse_args()
+    return dbengine.createProject(args)
