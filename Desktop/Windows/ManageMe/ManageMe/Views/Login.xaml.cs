069eb509200f0ad73737e054688b287b3c01da0f
Trent Killinger <trentkillinger@gmail.com>
2017-11-20 22:20:14 -0500

    Login and Register working

--- Desktop/Windows/ManageMe/ManageMe/Views/Login.xaml.cs
+++ Desktop/Windows/ManageMe/ManageMe/Views/Login.xaml.cs
@@ -33,6 +33,34 @@ namespace ManageMe.Views
             this.MouseDown += delegate { DragMove(); };
         }
 
+        public void ClearInputs()
+        {
+            textBoxCFirstName.Text = "";
+            textBoxCLastName.Text = "";
+            textBoxCUserName.Text = "";
+            textBoxCEmail.Text = "";
+            textBoxCPassword.Password = "";
+            textBoxCConfirmPassword.Password = "";
+        }
+
+        public bool AllInputsValid()
+        {
+            if (textBoxCFirstName.Text == "") return false;
+            if (textBoxCLastName.Text == "") return false;
+            if (textBoxCUserName.Text == "") return false;
+            if (textBoxCEmail.Text == "") return false;
+            if (textBoxCPassword.Password == "") return false;
+            if (textBoxCConfirmPassword.Password == "") return false;
+            if (imageCConfirmPasswordError.Visibility == Visibility.Visible) return false;
+            if (imageCPasswordError.Visibility == Visibility.Visible) return false;
+            if (imageCUserNameError.Visibility == Visibility.Visible) return false;
+            if (imageCEmailError.Visibility == Visibility.Visible) return false;
+            if (imageCLastNameError.Visibility == Visibility.Visible) return false;
+            if (imageCFirstNameError.Visibility == Visibility.Visible) return false;
+            return true;
+        }
+
+        #region Control Events
         private void buttonExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
@@ -41,7 +69,7 @@ namespace ManageMe.Views
         private void buttonShowRegister_Click(object sender, RoutedEventArgs e)
         {
             gridSignIn.Visibility = Visibility.Hidden;
-            this.Height = 475;
+            this.Height = 450;
             gridRegister.Visibility = Visibility.Visible;
         }
 
@@ -62,6 +90,16 @@ namespace ManageMe.Views
             {
                 placeHolderTextBoxCConfirmPassword.Visibility = Visibility.Visible;
             }
+
+            if (textBoxCConfirmPassword.Password == textBoxCPassword.Password)
+            {
+                imageCConfirmPasswordError.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                imageCConfirmPasswordError.Visibility = Visibility.Visible;
+                imageCConfirmPasswordError.ToolTip = new ToolTip() { Content = "Passwords do not match" };
+            }
         }
 
         private void textBoxCPassword_PasswordChanged(object sender, RoutedEventArgs e)
@@ -73,6 +111,16 @@ namespace ManageMe.Views
             else
             {
                 placeHolderTextBoxCPassword.Visibility = Visibility.Visible;
+            }
+
+            if (Utilities.Utilities.IsStrongPassword(textBoxCPassword.Password))
+            {
+                imageCPasswordError.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                imageCPasswordError.Visibility = Visibility.Visible;
+                imageCPasswordError.ToolTip = new ToolTip() { ToolTip = "Password must have 1 Upper , 1 special, 1 lower character, 1 digit, and be 8 characters long" };
             }
         }
 
@@ -90,25 +138,98 @@ namespace ManageMe.Views
 
         private async void buttonRegister_Click(object sender, RoutedEventArgs e)
         {
-            if(Utilities.Utilities.CheckPasswords(textBoxCPassword.Password,textBoxCConfirmPassword.Password))
+            if (AllInputsValid())
             {
-                if(Utilities.Utilities.CheckPasswordStrength(textBoxCPassword.Password))
+                var result = await Utilities.User.Register(textBoxCUserName.Text, textBoxCPassword.Password, textBoxCFirstName.Text, textBoxCLastName.Text, textBoxCEmail.Text);
+                if (result.Status)
                 {
-                    var result = await ManageMe.Utilities.User.Register(textBoxCUserName.Text, textBoxCPassword.Password, textBoxCFirstName.Text, textBoxCLastName.Text, textBoxCEmail.Text);
-                    if(result.Status)
+                    MessageBox.Show("You have successfully registered " + textBoxCFirstName.Text + " " + textBoxCLastName.Text + ". You may now sign in.");
+                    gridSignIn.Visibility = Visibility.Visible;
+                    this.Height = 400;
+                    gridRegister.Visibility = Visibility.Hidden;
+                    ClearInputs();
+                }
+                else
+                {
+                    if (result.message.username != "")
                     {
-                        buttonBack_Click(null, null);
-                    } else
-                    {
-                        MessageBox.Show(result.Message);
+                        imageCUserNameError.Visibility = Visibility.Visible;
+                        imageCUserNameError.ToolTip = new ToolTip() { Content = "Username already taken" };
                     }
+                    else
+                    {
+                        MessageBox.Show("There was an error registering, please try again.");
+                    }
+
                 }
             }
         }
 
-        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        private async void buttonLogin_Click(object sender, RoutedEventArgs e)
         {
-            user.Login(textBoxUserName.Text, textBoxPassword.Password);
+            var result = await user.Login(textBoxUserName.Text, textBoxPassword.Password);
+            if(result.Status)
+            {
+                MainWindow window = new MainWindow();
+                window.Show();
+            }
+            else
+            {
+                imageUserNameError.Visibility = Visibility.Visible;
+                imageUserNameError.ToolTip = new ToolTip() { Content = "Username or password is incorrect" };
+            }
         }
+
+        private void textBoxCFirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(textBoxCFirstName.Text.Length < 4)
+            {
+                imageCFirstNameError.Visibility = Visibility.Visible;
+                imageCFirstNameError.ToolTip = new ToolTip() { Content = "First name must be over 4 characters" };
+            } else
+            {
+                imageCFirstNameError.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void textBoxCLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBoxCLastName.Text.Length < 2)
+            {
+                imageCLastNameError.Visibility = Visibility.Visible;
+                imageCLastNameError.ToolTip = new ToolTip() { Content = "Last name must Be over 4 characters" };
+            }
+            else
+            {
+                imageCLastNameError.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void textBoxCUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBoxCUserName.Text.Length < 2)
+            {
+                imageCUserNameError.Visibility = Visibility.Visible;
+                imageCUserNameError.ToolTip = new ToolTip() { Content = "Username must be over 4 characters" };
+            }
+            else
+            {
+                imageCUserNameError.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void textBoxCEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(Utilities.Utilities.IsValidEmail(textBoxCEmail.Text))
+            {
+                imageCEmailError.Visibility = Visibility.Visible;
+                imageCEmailError.ToolTip = new ToolTip() { Content = "Not a valid email address" };
+            }
+            else
+            {
+                imageCEmailError.Visibility = Visibility.Hidden;
+            }
+        }
+        #endregion
     }
 }
