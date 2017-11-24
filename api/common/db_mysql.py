439db62285428285d607dcc8ffa26778d4021800
azaiter <azaiter@svsu.edu>
2017-11-23 22:53:06 -0800

    Jaelyn: DBEngine edits

--- api/common/db_mysql.py
+++ api/common/db_mysql.py
@@ -29,26 +29,6 @@ def getUser(args):
     db.close()
   return jsonify(r)
 
-  
-  
-'''
-To Do Jaelyn:
-2- implement login function
-      INPUT: args is a json object with the following information
-     
-      username
-      password
-      
-      OUTPUT: a json message and Token information
-	  example:
-	  {
-		  "access_token": "eyJhbGciOiJIUzI1NiIsI.eyJpc3MiOiJodHRwczotcGxlL.mFrs3Zo8eaSNcxiNfvRh9dqKP4F1cB",
-		  "expires_in":3600
-	  }
-      
-3- implement checkTokenIsValid function
-'''
-
 def checkTokenIsValid(token):
 	db = dbConnect()
 	cur = db.cursor()
@@ -90,8 +70,6 @@ def register(args):
 		db.close()
 	return jsonify(r[0])
   
-  
-  
 def checkUsernameExists(username):
 	db = dbConnect()
 	cur = db.cursor()
@@ -102,21 +80,7 @@ def checkUsernameExists(username):
 		db.commit()
 		db.close()
 	return len(r) > 0
-	
-	
-	
-	
-'''
-createProject
-	INPUT:
-		token
-		project_name
-		project_desc
-	OUTPUT:
-		Tuple of the project created.
-	
-	from the token you can check permissions and figure out "created_by" field.
-'''
+
 def createProject(args):
 	if checkTokenIsValid(args.token):
 		db = dbConnect()
@@ -130,30 +94,18 @@ def createProject(args):
 		print(r)
 	return jsonify(r)
 	
-	
-'''
-getProjects:
-	INPUT: 
-		token
-	OUTPUT:
-		all projects related to the user
-'''
 def getProjects(args):
-	returnTestData = [
-	{
-		"uid":1,
-		"name":"Test Project 1",
-		"desc":"Some Desc 1",
-		"created":"2017-11-15 08:43:24"
-	},
-	{
-		"uid":2,
-		"name":"Test Project 2",
-		"desc":"Some Desc 2",
-		"created":"2017-11-15 08:43:24"
-	}
-	]
-	return(jsonify(returnTestData))
+	if checkTokenIsValid(args.token):
+		db = dbConnect()
+		cur = db.cursor()
+		cur.callproc('sp_getProjects',[str(args.token)])
+		r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+		if len(r) == 0:
+			abort(404, message='No Projects are found for user!')
+		if db:
+			db.close()
+			print(r)
+	return jsonify(r)
 
 
 def checkProjectExist(projectName):
@@ -234,55 +186,51 @@ note that in the future, admin will send user_id to get other user's clocks.
 '''
 
 def getHours(args):
-	returnTestData = [
-	{
-		"uid":1,
-		"req_uid":"54", # Make the DB return dummy column for req_uid until we get into next iterations
-		"in":"2017-11-15 00:43:24",
-		"out":"2017-11-15 08:11:12",
-		"user_id":"3"
-	},
-	{
-		"uid":2,
-		"req_uid":"56", # Make the DB return dummy column for req_uid until we get into next iterations
-		"in":"2017-11-15 00:43:24",
-		"out":"2017-11-15 08:11:12",
-		"user_id":"3"
-	}
-	]
-	return(jsonify(returnTestData))
+	if checkTokenIsValid(args.token):
+		db = dbConnect()
+		cur = db.cursor()
+		cur.callproc('sp_viewTime',[str(args.token)])
+		r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+		if len(r) == 0:
+			abort(404, message='No time entries are found for user!')
+		if db:
+			db.close()
+			print(r)
+	return jsonify(r)
 
 '''
 IN: token
 OUT: the updated tuple (should have NULL out time.
 '''
 def clockIn(args):
-	returnTestData = [
-	{
-		"uid":1,
-		"req_uid":"54", # Make the DB return dummy column for req_uid until we get into next iterations
-		"in":"2017-11-15 00:43:24",
-		"out":"",
-		"user_id":"3"
-	}
-	]
-	return(jsonify(returnTestData))
+	if checkTokenIsValid(args.token):
+		db = dbConnect()
+		cur = db.cursor()
+		cur.callproc('sp_clockIn',[str(args.token), str(56)])
+		r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	if db:
+		cur.close()
+		db.commit()
+		db.close()
+		print(r)
+	return jsonify(r)
 
 '''
 IN: token
 OUT: the updated tuple
 '''
 def clockOut(args):
-	returnTestData = [
-	{
-		"uid":1,
-		"req_uid":"54", # Make the DB return dummy column for req_uid until we get into next iterations
-		"in":"2017-11-15 00:43:24",
-		"out":"",
-		"user_id":"3"
-	}
-	]
-	return(jsonify(returnTestData))
+	if checkTokenIsValid(args.token):
+		db = dbConnect()
+		cur = db.cursor()
+		cur.callproc('sp_clockOut',[str(args.token), str(56)])
+		r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	if db:
+		cur.close()
+		db.commit()
+		db.close()
+		print(r)
+	return jsonify(r)
 
   
 '''
@@ -305,9 +253,14 @@ IN: token, user_id
 OUT: a message
 '''
 def disableUser(args):
-	returnTestData = [
-	{
-		"message":"user has been disabled"
-	}
-	]
-	return(jsonify(returnTestData))
+	if checkTokenIsValid(args.token):
+		db = dbConnect()
+		cur = db.cursor()
+		cur.callproc('sp_disableUser',[str(args.user_id)])
+		r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+	if db:
+		cur.close()
+		db.commit()
+		db.close()
+		print(r)
+	return jsonify(r)
