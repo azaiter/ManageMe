d2c2710b81212e344762ee9cd124ce78b8e60f5d
Trent Killinger <trentkillinger@gmail.com>
2018-03-05 18:09:08 -0500

    Flippy
    Flippy da filp. :flipper:

--- new_web/src/components/pages/Login.js
+++ new_web/src/components/pages/Login.js
@@ -4,54 +4,53 @@ import {
   Button, CardHeader,
   CardBody, CardText,
 } from 'reactstrap';
-import FlipCard from 'react-flipcard-2';
+import ReactCardFlip from 'react-card-flip';
 import { withRouter } from 'react-router-dom';
 import { login, userIsLoggedIn } from '../../utils/Auth';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       isFlipped: false,
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  showBack() {
-    this.setState({
-      isFlipped: true,
-    });
-  }
-
-  showFront() {
-    this.setState({
-      isFlipped: false,
-    });
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ isFlipped: !this.state.isFlipped });
   }
 
   render() {
     return (
       <div id="loginCard">
-        <FlipCard disabled flipped={this.state.isFlipped}>
-          <div>
-            <Card >
-              <CardHeader className="bg-primary text-white">Login</CardHeader>
-              <CardBody>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button onClick={() => this.showBack()}>Go somewhere</Button>
-              </CardBody>
-            </Card>
-          </div>
-          <div>
-            <Card >
-              <CardHeader className="bg-primary text-white">Signup</CardHeader>
-              <CardBody>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button onClick={() => this.showFront()}>Go somewhere</Button>
-              </CardBody>
-            </Card>
-          </div>
-        </FlipCard>
+        <ReactCardFlip isFlipped={this.state.isFlipped}>
+          <Card key="front">
+            <CardHeader className="bg-primary text-white">Login</CardHeader>
+            <CardBody>
+              <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+              <Button onClick={this.handleClick}>Go somewhere</Button>
+            </CardBody>
+          </Card>
+          <Card key="back">
+            <CardHeader className="bg-primary text-white">Signup</CardHeader>
+            <CardBody>
+              <CardText>
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              With supporting text below as a natural lead-in to additional content.
+              </CardText>
+              <Button onClick={this.handleClick}>Go somewhere</Button>
+            </CardBody>
+          </Card>
+        </ReactCardFlip>
       </div>
     );
   }
