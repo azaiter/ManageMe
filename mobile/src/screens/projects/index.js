af6322e5ac00e818097d421079748207d2df9f0b
Zaiter <azaiter@svsu.edu>
2018-09-16 15:51:56 -0400

    create simple projects screen

--- mobile/src/screens/projects/index.js
+++ mobile/src/screens/projects/index.js
@@ -9,17 +9,47 @@ import {
   Right,
   Body,
   Text,
-  Icon
+  Icon,
+  List,
+  ListItem,
+  Card,
+  CardItem
 } from "native-base";
 import styles from "./styles";
 const Auth = require("../../util/Auth");
+const ApiCalls = require("../../util/ApiCalls");
 class Projects extends Component {
   constructor(props){
     super(props);
+    this.state = {};
     Auth.setIsLoginStateOnScreenEntry(this, {navigate:"Projects"});
   }
 
+  assignProjectsToState(opts={refresh:false}){
+    if (!this.state.projectsList || opts.refresh){
+      ApiCalls.getProjects().then(response=>{
+        ApiCalls.handleAPICallResult(response).then(apiResults=>{
+          if (apiResults){
+            this.setState({
+              projectsList:apiResults
+            });
+          }
+        });
+      });
+    }
+  }
+
+  getProjectsFromState(){
+    if (this.state && this.state.projectsList){
+      return this.state.projectsList;
+    }
+    else {
+      return [];
+    }
+  }
+
   render() {
+    this.assignProjectsToState();
     return (
       <Container style={styles.container}>
         <Header>
@@ -34,13 +64,41 @@ class Projects extends Component {
           <Body>
             <Title>Projects</Title>
           </Body>
-          <Right />
+          <Right>
+            <Button
+              transparent
+              onPress={() => this.assignProjectsToState({refresh:true})}
+            >
+              <Icon name="refresh" />
+            </Button>
+          </Right>
         </Header>
 
         <Content padder>
-          <Button onPress={() => this.props.navigation.goBack()}>
-            <Text>Back</Text>
-          </Button>
+          <List
+              dataArray={this.getProjectsFromState()}
+              renderRow={data =>
+                <ListItem>
+                  <Card style={styles.mb}>
+                    <CardItem bordered>
+                      <Left>
+                        <Body>
+                          <Text>{data.uid} - {data.name}</Text>
+                          <Text note>{data.created}</Text>
+                        </Body>
+                      </Left>
+                    </CardItem>
+
+                    <CardItem>
+                      <Body>
+                        <Text>
+                          Description: {data.desc}
+                        </Text>
+                      </Body>
+                    </CardItem>
+                  </Card>
+                </ListItem>}
+          />
         </Content>
       </Container>
     );
