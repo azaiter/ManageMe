7092c0c19eb4061070bf60d92e45ae3d75461a7e
alpero <kurasdaedron@gmail.com>
2018-09-22 14:25:05 -0400

    removed logs

--- mobile/src/util/ApiCalls.js
+++ mobile/src/util/ApiCalls.js
@@ -7,8 +7,6 @@ callFetch = async function (params) {
     if(params.url) {
         let tok = await Auth.getLocalToken();
         let bodyObj = (params.body)?params.body:params;
-        console.log("params",params);
-        console.log("bodyObj",bodyObj);
         if(bodyObj.paramArr) {
             if(bodyObj.paramArr.constructor === Array) {
                 bodyObj.paramArr = [tok.token].concat(bodyObj.paramArr);
