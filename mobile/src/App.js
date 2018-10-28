e861b25004902aa6a7ba218e8858ed2d7b03cd56
Kamalkumar Sekar <ksekar@svsu.edu>
2018-10-28 16:04:18 -0400

    Side Bar design Changes

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -2,123 +2,125 @@ import React from "react";
 import { Root } from "native-base";
 import { StackNavigator, DrawerNavigator } from "react-navigation";
 
-import Header from "./screens/Header/";
-import Header1 from "./screens/Header/1";
-import Header2 from "./screens/Header/2";
-import Header3 from "./screens/Header/3";
-import Header4 from "./screens/Header/4";
-import Header5 from "./screens/Header/5";
-import Header6 from "./screens/Header/6";
-import Header7 from "./screens/Header/7";
-import Header8 from "./screens/Header/8";
-import HeaderSpan from "./screens/Header/header-span";
-import HeaderNoShadow from "./screens/Header/header-no-shadow";
-import BasicFooter from "./screens/footer/basicFooter";
-import IconFooter from "./screens/footer/iconFooter";
-import IconText from "./screens/footer/iconText";
-import BadgeFooter from "./screens/footer/badgeFooter";
-import Default from "./screens/button/default";
-import Outline from "./screens/button/outline";
-import Rounded from "./screens/button/rounded";
-import Block from "./screens/button/block";
-import Full from "./screens/button/full";
-import Custom from "./screens/button/custom";
-import Transparent from "./screens/button/transparent";
-import IconBtn from "./screens/button/iconBtn";
-import Disabled from "./screens/button/disabled";
-import BasicCard from "./screens/card/basic";
-import NHCardItemBordered from "./screens/card/carditem-bordered";
-import NHCardItemButton from "./screens/card/carditem-button";
-import NHCardImage from "./screens/card/card-image";
-import NHCardShowcase from "./screens/card/card-showcase";
-import NHCardList from "./screens/card/card-list";
-import NHCardHeaderAndFooter from "./screens/card/card-header-and-footer";
-import NHCardTransparent from "./screens/card/card-transparent";
-import BasicFab from "./screens/fab/basic";
-import MultipleFab from "./screens/fab/multiple";
-import FixedLabel from "./screens/form/fixedLabel";
-import InlineLabel from "./screens/form/inlineLabel";
-import FloatingLabel from "./screens/form/floatingLabel";
-import PlaceholderLabel from "./screens/form/placeholder";
-import StackedLabel from "./screens/form/stacked";
-import RegularInput from "./screens/form/regular";
-import UnderlineInput from "./screens/form/underline";
-import RoundedInput from "./screens/form/rounded";
-import IconInput from "./screens/form/iconInput";
-import SuccessInput from "./screens/form/success";
-import ErrorInput from "./screens/form/error";
-import DisabledInput from "./screens/form/disabledInput";
-import Icons from "./screens/icon/icon";
-import BasicIcon from "./screens/icon/basic";
-import StateIcon from "./screens/icon/state";
-import PlatformSpecificIcon from "./screens/icon/platform-specific";
-import IconFamily from "./screens/icon/icon-family";
-import RowNB from "./screens/layout/row";
-import ColumnNB from "./screens/layout/column";
-import NestedGrid from "./screens/layout/nested";
-import CustomRow from "./screens/layout/customRow";
-import CustomCol from "./screens/layout/customCol";
-import BasicListSwipe from "./screens/listSwipe/basic-list-swipe";
-import SwipeRowCustomStyle from "./screens/listSwipe/swipe-row-style";
-import MultiListSwipe from "./screens/listSwipe/multi-list-swipe";
-import NHBasicList from "./screens/list/basic-list";
-import NHListItemSelected from "./screens/list/listitem-selected";
-import NHListDivider from "./screens/list/list-divider";
-import NHListSeparator from "./screens/list/list-separator";
-import NHListHeader from "./screens/list/list-headers";
-import NHListIcon from "./screens/list/list-icon";
-import NHListAvatar from "./screens/list/list-avatar";
-import NHListThumbnail from "./screens/list/list-thumbnail";
-import RegularPicker from "./screens/picker/regularPicker";
-import PickerWithIcon from "./screens/picker/picker-with-icon";
-import PlaceholderPicker from "./screens/picker/placeholderPicker";
-import PlaceholderPickerNote from "./screens/picker/placeholderPickernote";
-import BackButtonPicker from "./screens/picker/backButtonPicker";
-import PickerTextItemText from "./screens/picker/picker-text-itemtext";
-import HeaderPicker from "./screens/picker/headerPicker";
-import HeaderStylePicker from "./screens/picker/headerStylePicker";
-import CustomHeaderPicker from "./screens/picker/customHeaderPicker";
-import BasicTab from "./screens/tab/basicTab";
-import ConfigTab from "./screens/tab/configTab";
-import ScrollableTab from "./screens/tab/scrollableTab";
-import BasicSegment from "./screens/segment/SegmentHeader";
-import BasicToast from "./screens/toast/basic-toast";
-import ToastDuration from "./screens/toast/toast-duration";
-import ToastPosition from "./screens/toast/toast-position";
-import ToastType from "./screens/toast/toast-type";
-import ToastText from "./screens/toast/toast-text";
-import ToastButton from "./screens/toast/toast-button";
-import RegularActionSheet from "./screens/actionsheet/regular";
-import IconActionSheet from "./screens/actionsheet/icon";
-import AdvSegment from "./screens/segment/segmentTab";
-import SimpleDeck from "./screens/deckswiper/simple";
-import AdvancedDeck from "./screens/deckswiper/advanced";
+import DevSample from "./screens/devSample";
+import Header1 from "./screens/devSample/Header/1";
+import Header2 from "./screens/devSample/Header/2";
+import Header3 from "./screens/devSample/Header/3";
+import Header4 from "./screens/devSample/Header/4";
+import Header5 from "./screens/devSample/Header/5";
+import Header6 from "./screens/devSample/Header/6";
+import Header7 from "./screens/devSample/Header/7";
+import Header8 from "./screens/devSample/Header/8";
+import HeaderSpan from "./screens/devSample/Header/header-span";
+import HeaderNoShadow from "./screens/devSample/Header/header-no-shadow";
+import BasicFooter from "./screens/devSample/footer/basicFooter";
+import IconFooter from "./screens/devSample/footer/iconFooter";
+import IconText from "./screens/devSample/footer/iconText";
+import BadgeFooter from "./screens/devSample/footer/badgeFooter";
+import Default from "./screens/devSample/button/default";
+import Outline from "./screens/devSample/button/outline";
+import Rounded from "./screens/devSample/button/rounded";
+import Block from "./screens/devSample/button/block";
+import Full from "./screens/devSample/button/full";
+import Custom from "./screens/devSample/button/custom";
+import Transparent from "./screens/devSample/button/transparent";
+import IconBtn from "./screens/devSample/button/iconBtn";
+import Disabled from "./screens/devSample/button/disabled";
+import BasicCard from "./screens/devSample/card/basic";
+import NHCardItemBordered from "./screens/devSample/card/carditem-bordered";
+import NHCardItemButton from "./screens/devSample/card/carditem-button";
+import NHCardImage from "./screens/devSample/card/card-image";
+import NHCardShowcase from "./screens/devSample/card/card-showcase";
+import NHCardList from "./screens/devSample/card/card-list";
+import NHCardHeaderAndFooter from "./screens/devSample/card/card-header-and-footer";
+import NHCardTransparent from "./screens/devSample/card/card-transparent";
+import BasicFab from "./screens/devSample/fab/basic";
+import MultipleFab from "./screens/devSample/fab/multiple";
+import FixedLabel from "./screens/devSample/form/fixedLabel";
+import InlineLabel from "./screens/devSample/form/inlineLabel";
+import FloatingLabel from "./screens/devSample/form/floatingLabel";
+import PlaceholderLabel from "./screens/devSample/form/placeholder";
+import StackedLabel from "./screens/devSample/form/stacked";
+import RegularInput from "./screens/devSample/form/regular";
+import UnderlineInput from "./screens/devSample/form/underline";
+import RoundedInput from "./screens/devSample/form/rounded";
+import IconInput from "./screens/devSample/form/iconInput";
+import SuccessInput from "./screens/devSample/form/success";
+import ErrorInput from "./screens/devSample/form/error";
+import DisabledInput from "./screens/devSample/form/disabledInput";
+import Icons from "./screens/devSample/icon/icon";
+import BasicIcon from "./screens/devSample/icon/basic";
+import StateIcon from "./screens/devSample/icon/state";
+import PlatformSpecificIcon from "./screens/devSample/icon/platform-specific";
+import IconFamily from "./screens/devSample/icon/icon-family";
+import RowNB from "./screens/devSample/layout/row";
+import ColumnNB from "./screens/devSample/layout/column";
+import NestedGrid from "./screens/devSample/layout/nested";
+import CustomRow from "./screens/devSample/layout/customRow";
+import CustomCol from "./screens/devSample/layout/customCol";
+import BasicListSwipe from "./screens/devSample/listSwipe/basic-list-swipe";
+import SwipeRowCustomStyle from "./screens/devSample/listSwipe/swipe-row-style";
+import MultiListSwipe from "./screens/devSample/listSwipe/multi-list-swipe";
+import NHBasicList from "./screens/devSample/list/basic-list";
+import NHListItemSelected from "./screens/devSample/list/listitem-selected";
+import NHListDivider from "./screens/devSample/list/list-divider";
+import NHListSeparator from "./screens/devSample/list/list-separator";
+import NHListHeader from "./screens/devSample/list/list-headers";
+import NHListIcon from "./screens/devSample/list/list-icon";
+import NHListAvatar from "./screens/devSample/list/list-avatar";
+import NHListThumbnail from "./screens/devSample/list/list-thumbnail";
+import RegularPicker from "./screens/devSample/picker/regularPicker";
+import PickerWithIcon from "./screens/devSample/picker/picker-with-icon";
+import PlaceholderPicker from "./screens/devSample/picker/placeholderPicker";
+import PlaceholderPickerNote from "./screens/devSample/picker/placeholderPickernote";
+import BackButtonPicker from "./screens/devSample/picker/backButtonPicker";
+import PickerTextItemText from "./screens/devSample/picker/picker-text-itemtext";
+import HeaderPicker from "./screens/devSample/picker/headerPicker";
+import HeaderStylePicker from "./screens/devSample/picker/headerStylePicker";
+import CustomHeaderPicker from "./screens/devSample/picker/customHeaderPicker";
+import BasicTab from "./screens/devSample/tab/basicTab";
+import ConfigTab from "./screens/devSample/tab/configTab";
+import ScrollableTab from "./screens/devSample/tab/scrollableTab";
+import BasicSegment from "./screens/devSample/segment/SegmentHeader";
+import BasicToast from "./screens/devSample/toast/basic-toast";
+import ToastDuration from "./screens/devSample/toast/toast-duration";
+import ToastPosition from "./screens/devSample/toast/toast-position";
+import ToastType from "./screens/devSample/toast/toast-type";
+import ToastText from "./screens/devSample/toast/toast-text";
+import ToastButton from "./screens/devSample/toast/toast-button";
+import RegularActionSheet from "./screens/devSample/actionsheet/regular";
+import IconActionSheet from "./screens/devSample/actionsheet/icon";
+import AdvSegment from "./screens/devSample/segment/segmentTab";
+import SimpleDeck from "./screens/devSample/deckswiper/simple";
+import AdvancedDeck from "./screens/devSample/deckswiper/advanced";
+import TextArea from "./screens/devSample/form/textArea";
 
-import Anatomy from "./screens/anatomy/";
-import Footer from "./screens/footer/";
-import NHBadge from "./screens/badge/";
-import NHButton from "./screens/button/";
-import NHCard from "./screens/card/";
-import NHCheckbox from "./screens/checkbox/";
-import NHDeckSwiper from "./screens/deckswiper/";
-import NHFab from "./screens/fab/";
-import NHForm from "./screens/form/";
-import TextArea from "./screens/form/textArea";
-import NHIcon from "./screens/icon/";
-import ListSwipe from "./screens/listSwipe/";
-import NHLayout from "./screens/layout/";
-import NHList from "./screens/list/";
-import NHRadio from "./screens/radio/";
-import NHSearchbar from "./screens/searchbar/";
-import NHSpinner from "./screens/spinner/";
-import NHPicker from "./screens/picker/";
-import NHTab from "./screens/tab/";
-import NHThumbnail from "./screens/thumbnail/";
-import NHTypography from "./screens/typography/";
+import Anatomy from "./screens/devSample/anatomy/";
+import Header from "./screens/devSample/Header/";
+import Footer from "./screens/devSample/footer/";
+import NHBadge from "./screens/devSample/badge/";
+import NHButton from "./screens/devSample/button/";
+import NHCard from "./screens/devSample/card/";
+import NHCheckbox from "./screens/devSample/checkbox/";
+import NHDeckSwiper from "./screens/devSample/deckswiper/";
+import NHFab from "./screens/devSample/fab/";
+import NHForm from "./screens/devSample/form/";
+import NHIcon from "./screens/devSample/icon/";
+import NHLayout from "./screens/devSample/layout/";
+import NHList from "./screens/devSample/list/";
+import ListSwipe from "./screens/devSample/listSwipe/";
+import NHRadio from "./screens/devSample/radio/";
+import NHSearchbar from "./screens/devSample/searchbar/";
+import NHSpinner from "./screens/devSample/spinner/";
+import NHPicker from "./screens/devSample/picker/";
+import NHTab from "./screens/devSample/tab/";
+import NHThumbnail from "./screens/devSample/thumbnail/";
+import NHTypography from "./screens/devSample/typography/";
+import Segment from "./screens/devSample/segment";
+import NHToast from "./screens/devSample/toast/";
+import Actionsheet from "./screens/devSample/actionsheet";
+
 import SideBar from "./screens/sidebar";
-import Segment from "./screens/segment";
-import NHToast from "./screens/toast/";
-import Actionsheet from "./screens/actionsheet";
 
 import Home from "./screens/home/";
 import Projects from "./screens/projects";
@@ -154,6 +156,32 @@ const Drawer = DrawerNavigator(
     AddUser: { screen: AddUser },
 
     //dev screens
+    DevSample: { screen: DevSample },
+  },
+  {
+    initialRouteName: "Projects",
+    contentOptions: {
+      activeTintColor: "#e91e63"
+    },
+    contentComponent: props => <SideBar {...props} />
+  }
+);
+
+const AppNavigator = StackNavigator(
+  {
+    Drawer: { screen: Drawer },
+
+    // app screens
+    CreateProject: { screen: CreateProject },
+    ProjectInfo: { screen: ProjectInfo },
+    Requirements: { screen: Requirements },
+
+    CreateTeam: { screen: CreateTeam },
+    TeamMembers: { screen: TeamMembers },
+
+    AddUser: { screen: AddUser },
+
+    //dev screens
     Anatomy: { screen: Anatomy },
     Header: { screen: Header },
     Footer: { screen: Footer },
@@ -177,36 +205,7 @@ const Drawer = DrawerNavigator(
     NHTypography: { screen: NHTypography },
     Segment: { screen: Segment },
     NHToast: { screen: NHToast },
-    Actionsheet: { screen: Actionsheet }
-  },
-  {
-    initialRouteName: "Projects",
-    contentOptions: {
-      activeTintColor: "#e91e63"
-    },
-    contentComponent: props => <SideBar {...props} />
-  }
-);
-
-const AppNavigator = StackNavigator(
-  {
-    Drawer: { screen: Drawer },
-
-    // app screens
-    Home: { screen: Home },
-    Projects: { screen: Projects },
-    Teams: { screen: Teams },
-    Permissions: { screen: Permissions },
-    Users: { screen: Users },
-
-    CreateProject: { screen: CreateProject },
-    ProjectInfo: { screen: ProjectInfo },
-    Requirements: { screen: Requirements },
-
-    CreateTeam: { screen: CreateTeam },
-    TeamMembers: { screen: TeamMembers },
-
-    AddUser: { screen: AddUser },
+    Actionsheet: { screen: Actionsheet },
 
     Header1: { screen: Header1 },
     Header2: { screen: Header2 },
