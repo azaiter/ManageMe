89553677e0aaae688462b85e0c052c2e419e1062
alpero <kurasdaedron@gmail.com>
2018-11-19 23:22:10 -0500

    redefined standards

--- mobile/src/util/Render.js
+++ mobile/src/util/Render.js
@@ -7,49 +7,39 @@ import {
   Right,
   Body,
   Icon,
+  Text
 } from "native-base";
 
+/** HEADER **/
 export function ManageMe_Header(props) {
-  /*
-  props = {
-    title: "title",
-    leftIcon: "leftIcon",
-    onPress: {
-        left: () => { console.log("do stuff"); },
-        refresh: () => { console.log("do stuff"); },
-        add: () => { console.log("do stuff"); }
-    }
-  };
-  */
   return (
     <Header hasTabs>
       <Left>
-        <Button
-          transparent
-          onPress={props.onPress.left} // this changes
-        >
-          <Icon name={props.leftIcon} />
-        </Button>
+        <ManageMe_LeftButton />
       </Left>
       <Body>
         <Title>{props.title}</Title>
       </Body>
       <Right>
-        {
-          props.onPress.add ? 
-            <ManageMe_BackButton onPress={props.onPress.add}>
-            </ManageMe_BackButton> : null}
-        {props.onPress.refresh ?
-          <Button
-              transparent
-              onPress={props.onPress.refresh}
-            >
-            <Icon name="ios-refresh-circle" />
-          </Button>
-          : null
-        }
+        { props.onPress.add     ? <ManageMe_BackButton    onPress={props.onPress.add    }/> : null }
+        { props.onPress.refresh ? <ManageMe_RefreshButton onPress={props.onPress.refresh}/> : null }
       </Right>
     </Header>
+  );
+}
+
+export function ManageMe_LeftButton(props) {
+  return (
+    <Button
+      transparent
+      onPress={props.onPress.left}
+    >
+      <Icon name={
+        (props.leftButton === "back") ?
+          "ios-arrow-dropleft-circle" :
+          "ios-arrow-dropleft-circle"
+      }/>
+    </Button>
   );
 }
 
@@ -61,5 +51,30 @@ export function ManageMe_BackButton(props) {
     >
       <Icon name="ios-add-circle" />
     </Button>
+  );
+}
+
+export function ManageMe_RefreshButton(props) {
+  return (
+    <Button
+      transparent
+      onPress={props.onPress.refresh}
+    >
+      <Icon name="ios-refresh-circle" />
+    </Button>
+  );
+}
+
+/** BODY **/
+export function ManageMe_Body(props) {
+  return (
+    <Text>Not Yet Implemented</Text>
+  );
+}
+
+/** MODAL **/
+export function ManageMe_Modal(props) {
+  return (
+    <Text>Not Yet Implemented</Text>
   );
 }
